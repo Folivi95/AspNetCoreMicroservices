248ff983c76faaaa09ac6de80fbd4e27549505d7
Folivi Joshua Oluwaseun <joshua.folivi@gmail.com>
2021-09-21 12:09:40 +0100

    updated Catalog.API

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -16,7 +16,7 @@ namespace Catalog.API
             CreateHostBuilder(args).Build().Run();
         }
 
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
